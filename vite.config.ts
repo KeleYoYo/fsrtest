e7730a5a027e3487ac2e31ea3e55fb00e6604438
aliyun3370523150 <3332528007@qq.com>
2024-05-09 11:41:09 +0800

    初始化vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,44 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import VueDevTools from 'vite-plugin-vue-devtools'
+import {resolve} from 'path'
+
+import Components from 'unplugin-vue-components/vite';
+import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
+    VueDevTools(),
+    Components({
+      resolvers: [
+        AntDesignVueResolver({
+          importStyle: false, // css in js
+        }),
+      ],
+    }),
   ],
+  // resolve: {
+  //   alias: {
+  //     '@': fileURLToPath(new URL('./src', import.meta.url))
+  //   }
+  // },
   resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+    alias: [
+      {
+        find: '@',
+        replacement: resolve(__dirname, 'src')
+      },
+      {
+        find: '#',
+        replacement: resolve(__dirname, 'types')
+      }
+    ]
+  },
+  //antd样式不生效的解决方案
+  css:{
+    modules:false
   }
 })
